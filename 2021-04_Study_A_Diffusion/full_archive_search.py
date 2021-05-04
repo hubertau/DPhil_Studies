a5afbf1080f6f5e8852096ed7d72d2818dae7836
Hubert Au <hubertau@tutanota.com>
2021-05-04 16:07:39 +0100

    no csv output, just json for now

--- 2021-04_Study_A_Diffusion/full_archive_search.py
+++ 2021-04_Study_A_Diffusion/full_archive_search.py
@@ -19,6 +19,7 @@ with open('search_hashtags.csv', newline='') as f:
 # unroll list
 terms = [i[0] for i in terms]
 search_query = ' OR '.join(terms)
+assert len(search_query) <= 1024, 'Search query is above 1024 characters in length'
 
 search_url = "https://api.twitter.com/2/tweets/search/all"
 
@@ -26,17 +27,17 @@ search_url = "https://api.twitter.com/2/tweets/search/all"
 # expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
 query_params = {
                     'query': search_query,
-                    'tweet.fields': 'author_id,created_at',
-                    'start_time': '2017-10-17T00:00:00Z',
+                    'tweet.fields':'author_id,conversation_id,created_at,entities,geo,in_reply_to_user_id,lang,public_metrics,possibly_sensitive,referenced_tweets',
+                    'media.fields':'media_key,type,preview_image_url',
+                    'place.fields':'full_name,id,country,country_code,geo,name,place_type',
+                    'start_time':'2017-10-17T00:00:00Z',
                     'end_time': '2017-12-31T23:59:59Z',
                     'max_results':100
                }
 
-
 def create_headers(bearer_token):
     headers = {"Authorization": "Bearer {}".format(bearer_token)}
     return headers
-
 
 def connect_to_endpoint(url, headers, params):
     response = requests.request("GET", url, headers=headers, params=params)
@@ -61,12 +62,6 @@ def connect_to_endpoint(url, headers, params):
         logging.info('query parameters: {}\n'.format(params))
     return response.json()
 
-def try_field(tweet_result_obj, field_name):
-    try:
-        return tweet_result_obj[field_name]
-    except:
-        return 'NA'
-
 def main():
 
     # obtain current run time for reuslts
@@ -75,9 +70,11 @@ def main():
 
     # creating new path
     OUTPUT_PATH = os.path.join('collection_results_' + CURRENT_RUN_TIME)
+    DATA_PATH   = os.path.join(OUTPUT_PATH, 'data')
 
     # generate folder for output
     os.makedirs(OUTPUT_PATH, exist_ok=True)
+    os.makedirs(DATA_PATH, exist_ok=True)
 
     # set up logging file
     logging.basicConfig(filename=os.path.join(OUTPUT_PATH, 'full_archive_search.log'),
@@ -93,58 +90,42 @@ def main():
     print(start_time)
     logging.info('Full Archive Search Collection Start: {}'.format(start_time))
 
-    # create results file
-    results_path = os.path.join(OUTPUT_PATH, 'results.csv')
-    results_file = open(results_path, 'w')
-    results_writer = csv.writer(results_file)
-
     # create headers with bearer token
     headers = create_headers(bearer_token)
 
     # get first response
     count = 1
     json_response = connect_to_endpoint(search_url, headers, query_params)
+    file_to_save = json_response['data']
+    save_filename = os.path.join(DATA_PATH, 'FAS_' + str(count) + '.json')
+    with open(save_filename, 'w') as f:
+        json.dump(file_to_save, f)
 
-    # store results in csv format
-    results_header = ('author_id', 'text', 'created_at', 'id')
-    results_writer.writerow(results_header)
-    for tweet_result in json_response['data']:
-        result_row = (
-                try_field(tweet_result,'author_id'),
-                try_field(tweet_result,'text'),
-                try_field(tweet_result,'created_at'),
-                try_field(tweet_result,'id')
-            )       
-        results_writer.writerow(result_row)
-
+    file_to_save = []
     while 'next_token' in json_response['meta']:
         time.sleep(3)
         pagination_params = query_params
         pagination_params['next_token'] = json_response['meta']['next_token']
-        logging.info(json_response['meta']['next_token'])
+        logging.info('nexttoken:{}'.format(json_response['meta']['next_token']))
         json_response = connect_to_endpoint(search_url, headers, pagination_params)
         if 'data' not in json_response:
             logging.info(json_response)
             continue
-        for tweet_result in json_response['data']:
-            result_row = (
-                try_field(tweet_result,'author_id'),
-                try_field(tweet_result,'text'),
-                try_field(tweet_result,'created_at'),
-                try_field(tweet_result,'id')
-            )
-            results_writer.writerow(result_row)
         count += 1
-        if count % 100 == 0:
+        file_to_save.extend(json_response['data'])
+        if count % 200 == 0:
             logging.info(count)
-
-    # close writing to results
-    results_file.close()
+            save_filename = os.path.join(DATA_PATH, 'FAS_' + str(count) + '.json')
+            with open(save_filename, 'w') as f:
+                json.dump(file_to_save, f)
+            file_to_save = []
+            if count == 200:
+                break
 
     # data collection end time
     end_time = datetime.datetime.now()
     print(end_time)
-    logging.info('End Time: {}'.foramt(end_time))
+    logging.info('End Time: {}'.format(end_time))
     logging.info('Time Elapsed: {}'.format(end_time-start_time))
 
 if __name__ == "__main__":
