6dd59f3ab799338e05be4ff5abef0f723b5fbd12
Hubert Au <hubertau@tutanota.com>
2021-05-06 16:20:51 +0100

    twarc timelines

---
+++ 2021-04_Study_A_Diffusion/user_twarc_timelines.py
@@ -0,0 +1,46 @@
+import subprocess
+import os
+import argparse 
+import logging
+
+def main():
+
+    with open(args.user_list) as f:
+        users = f.readlines()
+
+    for user_id in users:
+
+        save_filename = os.path.join(args.output_dir,'data/timeline_' + user_id + '.jsonl')
+
+        subprocess.run(
+            ['twarc2',
+            'timeline',
+            '--end-time 2017-12-13T23:59:59',
+            user_id,
+            save_filename]
+        )
+
+        logging.info(user_id)
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='timeline collection')
+
+    parser.add_argument(
+        'user_list',
+        help='list of users in a txt file.'
+    )
+
+    parser.add_argument(
+        '--output_dir',
+        help='directory to place outputs'
+    )
+
+    args = parser.parse_args()
+
+    logging.basicConfig(filename=os.path.join(args.output_dir, 'user_timelines.log'),
+                        encoding='utf-8',
+                        format='%(levelname)s:%(message)s',
+                        level=logging.DEBUG)
+
+    main()
