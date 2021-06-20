cd34cec69e78c664136face9ba997b8b08b77c4e
Hubert Au <hubertau@tutanota.com>
2021-06-20 11:24:18 +0100

    fix type comparison

--- 2021-04_Study_A_Diffusion/full_archive_search.py
+++ 2021-04_Study_A_Diffusion/full_archive_search.py
@@ -115,7 +115,8 @@ class FAS_Collector(object):
             self.current_start = new_start
             self.current_end   = new_end
 
-            if new_start > datetime_from_string(self.end_time):
+            # if new_start>specified end time
+            if datetime_from_string(latest_file) > datetime_from_string(self.end_time):
                 self.save_filename = None
 
     @time_function
