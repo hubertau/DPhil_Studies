8fa3e6d13a31475f138b2afd8948eaecaf8637cc
Hubert Au <hubert.au@oii.ox.ac.uk>
2021-10-26 14:01:43 +0100

    additionally plot cdf of timeline completion

--- 2021-04_Study_A_Diffusion/src/d07_visualisation/plot_timeline_completeness.py
+++ 2021-04_Study_A_Diffusion/src/d07_visualisation/plot_timeline_completeness.py
@@ -5,11 +5,36 @@ import pickle
 import os
 import pandas as pd
 
+
+def generate_cdf_values(data):
+
+    # Get the frequency, PDF and CDF for each value in the series
+
+    data = pd.Series(data, name = 'value')
+    df = pd.DataFrame(data)
+
+    # Frequency
+    stats_df = df \
+    .groupby('value') \
+    ['value'] \
+    .agg('count') \
+    .pipe(pd.DataFrame) \
+    .rename(columns = {'value': 'frequency'})
+
+    # PDF
+    stats_df['pdf'] = stats_df['frequency'] / sum(stats_df['frequency'])
+
+    # CDF
+    stats_df['cdf'] = stats_df['pdf'].cumsum()
+    stats_df = stats_df.reset_index()
+    return stats_df
+
 def main():
 
     selected_date_ranges_file = '../../data/02_intermediate/FAS_selected_date_ranges.obj'
     timeline_completeness_pickle_fname = '../../data/02_intermediate/timeline_completeness.obj'
     plot_save_file = '../../results/plot_timeline_completeness.png'
+    cdf_plot_save_file = '../../results/plot_timeline_completeness_cdf.png'
 
     # first, how many users are incomplete?
 
@@ -30,6 +55,14 @@ def main():
     print('Saved at {}'.format(plot_save_file))
 
     # pd.DataFrame([i[1] for i in timeline_completeness]).describe(datetime_is_numeric=True)
+
+
+    # plot cdf
+    stats_df = generate_cdf_values([i[1] for i in timeline_completeness])
+
+    f = plt.figure(figsize=(25,15))
+    stats_df.plot(x = 'value', y = 'cdf', grid = True)
+    plt.savefig(cdf_plot_save_file)
 
 if __name__ == '__main__':
     main()
