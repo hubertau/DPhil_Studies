e39e34a4d048822455de6ec5cb77897417dd986e
Hubert Au <hubert.au@oii.ox.ac.uk>
2021-11-10 09:54:55 +0000

    bispectral search bash script update

--- 2021-04_Study_A_Diffusion/src/d04_modelling/bispec_clustering_search_run_python.sh
+++ 2021-04_Study_A_Diffusion/src/d04_modelling/bispec_clustering_search_run_python.sh
@@ -4,9 +4,8 @@
   ../../data/02_intermediate/01_group/ \
   --output_dir ../../data/05_model_output/01_group/ \
   --implementation python \
-  --range 100-500 \
+  --range 505-750 \
   --interval 5 \
   --min_user 100 \
-  --verbose
 
-mail -s "python clustering complete" hubert.au@oii.ox.ac.uk
+ls -alht ../../data/05_model_output/01_group/ | head -n 50 | mail -s "test" hubert.au@oii.ox.ac.uk
