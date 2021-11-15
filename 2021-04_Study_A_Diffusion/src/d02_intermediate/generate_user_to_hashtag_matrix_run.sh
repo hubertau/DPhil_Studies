d194f76abf9721b1c4319ecf5d77d83c16481449
Hubert Au <hubert.au@oii.ox.ac.uk>
2021-11-15 12:55:34 +0000

    made low memory default

--- 2021-04_Study_A_Diffusion/src/d02_intermediate/generate_user_to_hashtag_matrix_run.sh
+++ 2021-04_Study_A_Diffusion/src/d02_intermediate/generate_user_to_hashtag_matrix_run.sh
@@ -4,7 +4,6 @@
   ../../data/01_raw/ \
   ../../data/02_intermediate/01_group/ \
   --ngram_range 34 \
-  --low_memory \
   --verbose
 
 ls -alht ../../data/02_intermediate/01_group/ | mail -s "vectorizer complete" hubert.au@oii.ox.ac.uk
