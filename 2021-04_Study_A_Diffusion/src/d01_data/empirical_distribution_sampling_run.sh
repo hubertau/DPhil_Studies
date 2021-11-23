9da94b4e449eef2c14d3bff574958d8853e1e789
Hubert Au <hubert.au@oii.ox.ac.uk>
2021-11-23 14:58:33 +0000

    conversion to hdf5 file storage

--- 2021-04_Study_A_Diffusion/src/d01_data/empirical_distribution_sampling_run.sh
+++ 2021-04_Study_A_Diffusion/src/d01_data/empirical_distribution_sampling_run.sh
@@ -7,6 +7,7 @@
   2 \
   ../../data/02_intermediate/user_list_full_1_nocount.txt \
   --verbose \
-  --max_total 5000
+  --group_daterange_file /home/hubert/DPhil_Studies/2021-04_Study_A_Diffusion/data/02_intermediate/FAS_peak_analysis.hdf5 \
+  --max_total 2500
 
 ls -alht ../../data/02_intermediate/02_group/ | mail -s "empirical sampling complete" hubert.au@oii.ox.ac.uk
