7d5f55939e9cd262f77be488f64d43769bfd35fd
Hubert Au <hubert.au@oii.ox.ac.uk>
2021-11-24 17:30:20 +0000

    updated

--- 2021-04_Study_A_Diffusion/src/d01_data/user_timeline_collection_group_2_first_sample.sh
+++ 2021-04_Study_A_Diffusion/src/d01_data/user_timeline_collection_group_2_first_sample.sh
@@ -3,11 +3,14 @@
 # 2021-09-20: used for the full data collection.
 
 python3.9 user_twarc_timelines.py \
-    ../../data/02_intermediate/user_list_full_0_.txt \
-    --output_dir ../../data/02_group/ \
-    --log_dir ../../logs/
-    --start_time 2017-11-05T00:00:00 \
-    --end_time 2018-05-29T00:00:00 \
+    ../../data/02_intermediate/02_group/group_2_sampled_weight_False_users.txt \
+    --output_dir ../../data/01_raw/02_group/ \
+    --log_dir ../../logs/ \
+    --log_level DEBUG \
+    --start_time 2018-11-21T00:00:00 \
+    --end_time 2019-01-02T00:00:00 \
     --limit 25000 \
     --omit_hashtags \
     --omit_retweets
+
+ls -alht ../../data/01_raw/02_group/ | mail -s "group 2 first collection complete" hubert.au@oii.ox.ac.uk
