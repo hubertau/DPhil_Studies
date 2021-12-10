8af9e7ce405b9d462986db692c7c5afc07d13348
Hubert Au <hubert.au@oii.ox.ac.uk>
2021-12-10 11:34:39 +0000

    add group_num bash

--- 2021-04_Study_A_Diffusion/src/d00_utils/get_user_m3inference_run.sh
+++ 2021-04_Study_A_Diffusion/src/d00_utils/get_user_m3inference_run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+group_num=2
+
 python3.9 get_user_m3inference.py \
     --data_dirs ../../data/01_raw/ \
     --output_dir ../../data/03_processed/ \
-    --group_num 1 \
+    --group_num ${group_num} \
     --log_dir ../../logs/
+
+ls -alht ../../data/03_processed/ | mail -s "m3 ${group_num} complete" hubert.au@oii.ox.ac.uk
