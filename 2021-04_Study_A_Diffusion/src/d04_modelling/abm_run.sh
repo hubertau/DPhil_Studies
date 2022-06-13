b280dcb525b4431c6162d0cedd8299107e7a6196
Hubert Au <hubert.au@oii.ox.ac.uk>
2022-06-13 10:32:57 +0100

    updated

--- 2021-04_Study_A_Diffusion/src/d04_modelling/abm_run.sh
+++ 2021-04_Study_A_Diffusion/src/d04_modelling/abm_run.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/bash
 
-python3.9 abm.py \
+kernprof -l abm.py \
     --search_hashtags /home/hubert/DPhil_Studies/2021-04_Study_A_Diffusion/references/search_hashtags.txt \
     --group_num 2 \
     --data_path /home/hubert/DPhil_Studies/2021-04_Study_A_Diffusion/data/ \
     --activity_file /home/hubert/DPhil_Studies/2021-04_Study_A_Diffusion/data/03_processed/activity_counts.hdf5 \
     --output_path /home/hubert/DPhil_Studies/2021-04_Study_A_Diffusion/data/06_reporting \
-    --log_dir /home/hubert/DPhil_Studies/2021-04_Study_A_Diffusion/logs
+    --log_dir /home/hubert/DPhil_Studies/2021-04_Study_A_Diffusion/logs \
+    --params_file params.json \
+    --max_workers 2 \
+    --line_profiler \
+    --debug_len 10
 
