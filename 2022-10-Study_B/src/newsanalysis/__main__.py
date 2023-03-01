dec2454fbcaa109421d843fc02668e7c26b7ab8a
Hubert Au <hubert.au@oii.ox.ac.uk>
2023-03-01 16:02:30 +0000

    intercept loggin

--- 2022-10-Study_B/src/newsanalysis/__main__.py
+++ 2022-10-Study_B/src/newsanalysis/__main__.py
@@ -15,6 +15,26 @@ from pprint import PrettyPrinter
 import h5py
 import pandas as pd
 from collections import Counter, defaultdict
+import logging
+
+# intercept default logging package from submodules (e.g. SentenceTrasnformers)
+class InterceptHandler(logging.Handler):
+    def emit(self, record):
+        # Get corresponding Loguru level if it exists
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        # Find caller from where originated the logged message
+        frame, depth = logging.currentframe(), 2
+        while frame.f_code.co_filename == logging.__file__:
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
+
+logging.basicConfig(handlers=[InterceptHandler()], level=0)
 
 @click.group()
 @click.pass_context
