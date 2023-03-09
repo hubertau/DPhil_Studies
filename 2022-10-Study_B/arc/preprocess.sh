95717ed710a1461f2ad1c585fa9ce43581b5dff7
Hubert Au <hubertau@tutanota.com>
2023-03-09 10:37:25 +0000

    arc

--- 2022-10-Study_B/arc/preprocess.sh
+++ 2022-10-Study_B/arc/preprocess.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 #SBATCH --nodes=1
-#SBATCH --partition=short
-#SBATCH --time=1:00:00
+#SBATCH --partition=medium
+#SBATCH --time=48:00:00
 #SBATCH --job-name=B-preprocess
 #SBATCH --clusters=htc
 #SBATCH --gres=gpu:1
-#SBATCH --output=/home/ball4321/DPhil_Studies/2022-10-Study_B/logs/%A.log
+#SBATCH --mem-per-cpu=350G
+#SBATCH --output=/home/ball4321/DPhil_Studies/2022-10-Study_B/logs/preprocess-%A.log
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=hubert.au@oii.ox.ac.uk
 
@@ -20,5 +21,5 @@ source activate $DATA/venv_b
 cd /home/ball4321/DPhil_Studies/2022-10-Study_B/src
 
 python -m newsanalysis --gpu duplicate-check \
-  $DATA/data_b/01_raw/data.jsonl \
-  $DATA/data/03_processed/preprocessing/
+  $DATA/data_b/01_raw/data_ndid.jsonl \
+  $DATA/data_b/03_processed/preprocessing/
