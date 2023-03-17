608cf7da3daff2cf7ae0fd2f4939e53ac0ece431
Hubert Au <hubert.au@oii.ox.ac.uk>
2023-03-17 15:27:30 +0000

    updated tests

--- 2022-10-Study_B/tests/test_preprocess.py
+++ 2022-10-Study_B/tests/test_preprocess.py
@@ -7,7 +7,7 @@ def test_embeddings(tmp_path):
         'newsanalysis',
         'preprocess',
         'embed',
-        '../data/01_raw/data_ndid.jsonl',
+        '../data/01_raw/data_cleaned.jsonl',
         tmp_path,
         '-u',
         '50',
@@ -23,7 +23,7 @@ def test_obtain_clusters(tmp_path):
         'newsanalysis',
         'preprocess',
         'obtain-clusters',
-        '../data/01_raw/data_ndid.jsonl',
+        '../data/01_raw/data_cleaned.jsonl',
         tmp_path,
         '-u',
         '50',
@@ -31,4 +31,4 @@ def test_obtain_clusters(tmp_path):
         '5'
     ])
     end_files = glob.glob(os.path.join(tmp_path, '*'))
-    assert len(end_files) == 2
+    assert len(end_files) == 1
