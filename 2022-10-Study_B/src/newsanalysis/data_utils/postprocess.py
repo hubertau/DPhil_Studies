4d723bdb080b29cf23bb8199a4ff2915b82f9678
Hubert Au <hubert.au@oii.ox.ac.uk>
2023-06-22 13:46:53 +0100

    sts and cimpact first commit

--- 2022-10-Study_B/src/newsanalysis/data_utils/postprocess.py
+++ 2022-10-Study_B/src/newsanalysis/data_utils/postprocess.py
@@ -9,6 +9,16 @@ from itertools import repeat
 from datasets import Dataset
 from pathlib import Path
 import pickle
+import numpy as np
+import tensorflow_probability as tfp
+from tensorflow_probability import distributions as tfd
+from tensorflow_probability import sts
+import tensorflow as tf
+from dateutil.relativedelta import relativedelta
+from datetime import datetime
+from causalimpact import CausalImpact
+from causalimpact.misc import standardize
+
 
 import pickle
 from pathlib import Path
@@ -175,3 +185,207 @@ def consolidatener(ner_file, outfile, dataset, names, surnames, up_to):
     with open(outfile, 'wb') as f:
         pickle.dump(final_dict, f)
     logger.info(f'Saved to {outfile}')
+
+
+def extract_trend(complete_df, country, min_count = 500, resample_time = 'W'): 
+
+    reg_df = complete_df.copy()
+    reg_df = reg_df.set_index('publish_date')
+    reg_df = reg_df.groupby('country').resample(resample_time).agg(
+        {'processed_stories_id':'count', 'myth_total':'sum'}
+    ).reset_index()
+    reg_df.rename({'processed_stories_id':'count'}, axis=1, inplace=True)
+
+    if country in ['XXX', 'ZZZ']:
+        return None
+
+    obs_data = reg_df[reg_df['country']==country]['myth_total']
+    coverage = reg_df[reg_df['country']==country]['count']
+    if coverage.sum() < min_count:
+        return None
+
+    impact = tfp.sts.LocalLinearTrend(
+        observed_time_series=obs_data, name = f'#MeToo Trend - {country}'
+    )
+    coverage_effect = tfp.sts.LinearRegression(
+        design_matrix=tf.reshape(coverage - np.mean(coverage), (-1, 1)), name='Coverage Effect'
+    )
+    residual_level = tfp.sts.Autoregressive(
+        order=1,
+        observed_time_series=obs_data,
+        name='Residual'
+    )
+    model = tfp.sts.Sum([impact, coverage_effect, residual_level],
+                        observed_time_series=obs_data)
+
+    # Build the variational surrogate posteriors `qs`.
+    variational_posteriors = tfp.sts.build_factored_surrogate_posterior(
+        model=model)
+    
+
+    # Allow external control of optimization to reduce test runtimes.
+    num_variational_steps = 200 # @param { isTemplate: true}
+    num_variational_steps = int(num_variational_steps)
+
+    # Build and optimize the variational loss function.
+    elbo_loss_curve = tfp.vi.fit_surrogate_posterior(
+        target_log_prob_fn=model.joint_distribution(
+            observed_time_series=obs_data.log_prob,
+        surrogate_posterior=variational_posteriors,
+        optimizer=tf.optimizers.Adam(learning_rate=0.1),
+        num_steps=num_variational_steps,
+        jit_compile=True)
+    )
+
+    # plt.plot(elbo_loss_curve)
+    # plt.show()
+
+    # Draw samples from the variational posterior.
+    param_samples = variational_posteriors.sample(50)
+
+    component_dists = sts.decompose_by_component(
+        model,
+        observed_time_series=obs_data,
+        parameter_samples=param_samples
+    )
+
+    return country, model, elbo_loss_curve, param_samples, component_dists 
+
+
+@click.command()
+@click.argument('original_df_file')
+@click.argument('outdir')
+@click.option('--resample', '-r', help='timeframe to resample. Can be W, M, or Y', default='W')
+@click.option('--min_count', '-m', type=int, default=500)
+def sts(original_df_file, outdir, resample, min_count):
+
+    complete_df = pd.read_pickle(original_df_file)
+    logger.info(f'Data loaded in from {original_df_file}')
+
+    countries = list(complete_df['country'].unique())
+    logger.info(f'Unique contries collected')
+
+    logger.info(f'Begin ProcessPoolExecutor')
+    with ProcessPoolExecutor() as executor:
+        results = executor.map(
+            extract_trend,
+            repeat(complete_df),
+            countries,
+            repeat(min_count),
+            repeat(resample)
+        )
+
+    logger.info(f'End ProcessPoolExecutor')
+
+    results = [i for i in results]
+
+    outfile = Path(outdir) / f'sts_results_{min_count}_{resample}.pkl'
+    with open(outfile, 'wb') as f:
+        pickle.dump(results, f)
+
+
+def cimpact(complete_df, country, peaks, min_count = 500, resample_time = 'W'):
+    # N.B.  The linear regression must contain the union of pre and post data as required by TensorFlow Probability.
+
+    peak = peaks.loc[country, 'peak_date']
+
+    resample_time='M'
+
+    reg_df = complete_df.copy()
+    reg_df = reg_df.set_index('publish_date')
+    reg_df = reg_df.groupby('country').resample(resample_time).agg(
+        {
+            'processed_stories_id':'count',
+            'myth_total':'sum',
+            'e_v2x_gender_5C': 'first', # Women political empowerment
+        }
+    ).reset_index().set_index('publish_date').sort_index()
+
+    if resample_time == 'D':
+        # pre_period=['2014-10-17', '2017-10-17']
+        # post_period=['2017-10-18', '2020-10-17']
+        pre_period = ['2014-10-17', peak]
+        post_period = [
+            (peak + relativedelta(days=+1)).strftime("%Y-%m-%d"),
+            (peak + relativedelta(years=+1)).strftime("%Y-%m-%d")
+        ]
+    elif resample_time == 'M':
+        pre_period = ['2014-10-17', peak]
+        post_period = [
+            (peak + relativedelta(month=+1)).strftime("%Y-%m-%d"),
+            (peak + relativedelta(years=+1)).strftime("%Y-%m-%d")
+        ]
+    elif resample_time == 'W':
+        pre_period = ['2014-10-17', peak]
+        post_period = [
+            (peak + relativedelta(week=+1)).strftime("%Y-%m-%d"),
+            (peak + relativedelta(years=+1)).strftime("%Y-%m-%d")
+        ]
+
+    data = reg_df[reg_df['country']==country][['myth_total', 'processed_stories_id']]
+    normed_data, mu_sig = standardize(data)
+    # normed_data = data
+
+    obs_data = tfp.sts.regularize_series(normed_data['myth_total'].loc[:pre_period[1]].astype(np.float32))
+
+    design_matrix = tf.reshape(pd.concat([
+        normed_data['processed_stories_id'].loc[pre_period[0]: pre_period[1]],
+        normed_data['processed_stories_id'].loc[post_period[0]: post_period[1]]
+    ]).astype(np.float32), (-1,1))
+
+
+    linear_level = tfp.sts.LocalLinearTrend(observed_time_series=obs_data, name='local')
+    coverage_effect = tfp.sts.LinearRegression(design_matrix=design_matrix, name='coverage_effect')
+    month_season = tfp.sts.Seasonal(num_seasons=4, num_steps_per_season=1, observed_time_series=obs_data, name='Month')
+    year_season = tfp.sts.Seasonal(num_seasons=52, observed_time_series=obs_data, name='Year')
+    residual_level = tfp.sts.Autoregressive(
+        order=1,
+        observed_time_series=obs_data, name='residual')
+
+
+    model = tfp.sts.Sum([linear_level, coverage_effect, residual_level], observed_time_series=obs_data)
+
+    ci = CausalImpact(
+        normed_data,
+        pre_period,
+        post_period,
+        model=model,
+        model_args = {'fit_method': 'hmc'}
+    )
+    return country, ci
+
+@click.command()
+@click.argument('original_df_file')
+@click.argument('outdir')
+@click.option('--peaks', '-p', default = None)
+@click.option('--resample', '-r', help='timeframe to resample. Can be W, M, or Y', default='W')
+@click.option('--min_count', '-m', type=int, default=500)
+def ci(original_df_file, outdir, peaks, resample, min_count):
+    
+    complete_df = pd.read_pickle(original_df_file)
+    logger.info(f'Data loaded in from {original_df_file}')
+
+    if peaks:
+        peaks_df = pd.read_csv(peaks).set_index('country')
+
+    countries = list(complete_df['country'].unique())
+    logger.info(f'Unique contries collected')
+
+    logger.info(f'Begin ProcessPoolExecutor')
+    with ProcessPoolExecutor() as executor:
+        results = executor.map(
+            cimpact,
+            repeat(complete_df),
+            countries,
+            peaks_df,
+            repeat(min_count),
+            repeat(resample)
+        )
+
+    logger.info(f'End ProcessPoolExecutor')
+
+    results = [i for i in results]
+
+    outfile = Path(outdir) / f'cimpact_results_{min_count}_{resample}.pkl'
+    with open(outfile, 'wb') as f:
+        pickle.dump(results, f) 
